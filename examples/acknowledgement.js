e1342b825aff0ece7ab1da9cb452791a11a7ac9b
Michael Vergoz <m.vergoz@vergoz.ch>
2019-06-10 19:59:41 +0200

    Some ES6 port + low level authentification using psk

--- examples/acknowledgement.js
+++ examples/acknowledgement.js
@@ -3,19 +3,20 @@
 var Server = require('../index').Server;
 var Socket = require('../index').Socket;
 
-var server = new Server();
+var server = new Server({psk: "test"});
 server.on('connection', function (socket) {
-  socket.on('login', function (username, callback) {
-    callback(username === 'alex' ? true : false);
-  });
+	socket.on('login', function (username, callback) {
+		callback(username === 'alex' ? true : false);
+	});
 });
 
 server.listen(5000);
 
 var socket = new Socket({
-  host: 'localhost',
-  port: 5000
+	host: 'localhost',
+	port: 5000,
+	psk: "test"
 });
 socket.emit('login', 'alex', function (response) {
-  console.log('Response: ' + response);
+	console.log('Response: ' + response);
 });
